1dabfb437935b783d9993e605d5bdcd72e769b17
Alexandr Ezhuchenkov <ezhuchenkov@gmail.com>
2019-12-07 23:08:37 +0300

    итоги по второй части диплома

---
+++ src/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+import './vendor/normalize.css'
+import './index.css'
+
---
+++ src/pages/about/index.js
@@ -0,0 +1,4 @@
+import '../../vendor/normalize.css'
+import './index.css'
+import Flickity from 'flickity'
+

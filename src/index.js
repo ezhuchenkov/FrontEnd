56cbbf5b0d44850a297fe1681f7f5b8b62fbc7a8
Alexandr Ezhuchenkov <ezhuchenkov@gmail.com>
2019-12-10 01:55:06 +0300

    js формы регистрации

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@
 import './vendor/normalize.css'
 import './index.css'
 
+import Popup from "../src/blocks/popup/popup";
+
+const popupNew = new Popup(document.querySelector('#registration'));
+
+document.querySelector('.menu__link_logged').addEventListener('click', function (event) {
+  popupNew.open(event);});
